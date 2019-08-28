d37b5a0111f3ec3d783d28100604070d4232ab20
Grisel Alpanes <griselmelina@gmail.com>
2019-08-28 14:09:01 +0100

    Add Appointment Type seccion. Show date with a better format. Rename consultant type in the state. Show user information from API.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ class App extends Component {
 
     this.state = {
       userId: 1,
-      selectedAppointmentType: 'gp',
+      selectedConsultantType: 'gp',
       availableSlots: [],
+      selectedTime: null,
+      selectedAppointmentType: null,
+      user: null,
     }
   }
 
@@ -25,6 +28,38 @@ class App extends Component {
       .catch(() => {
         // TODO: Handle error here
       })
+    fetch(`${API_ENDPOINT}/users/${this.state.userId}`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ user: json })
+      })
+      .catch(() => {
+        // TODO: Handle error here
+      })
+  }
+
+  formatDatetime(value) {
+    const d = new Date(value)
+    const current_datetime = new Date()
+    if (
+      current_datetime.getFullYear() === d.getFullYear() &&
+      current_datetime.getMonth() === d.getMonth() &&
+      current_datetime.getDate() === d.getDate()
+    ) {
+      return 'Today ' + d.getHours() + ':' + d.getMinutes()
+    }
+
+    return (
+      d.getDate() +
+      '/' +
+      (d.getMonth() + 1) +
+      '/' +
+      d.getFullYear() +
+      ' ' +
+      d.getHours() +
+      ':' +
+      d.getMinutes()
+    )
   }
 
   render() {
@@ -38,45 +73,89 @@ class App extends Component {
       ) {
         if (
           this.state.availableSlots[i]['consultantType'][j] ===
-          this.state.selectedAppointmentType
+          this.state.selectedConsultantType
         ) {
           slots.push(this.state.availableSlots[i])
+        }
+      }
+    }
+    const current_datetime = new Date()
+    slots = slots.filter(slot => new Date(slot.time) > current_datetime)
+    slots.sort((slot1, slot2) => new Date(slot1.time) - new Date(slot2.time))
+    let availableSlotsForTime = []
+    for (let i = 0; i < slots.length; i++) {
+      for (let j = 0; j < slots[i]['appointmentType'].length; j++) {
+        if (slots[i]['time'] === this.state.selectedTime) {
+          availableSlotsForTime.push(slots[i]['appointmentType'][j])
         }
       }
     }
 
     return (
       <div className="app">
-        <h2 className="h6">New appointment</h2>
         <div className="app-header">
           <img src={logo} className="app-logo" alt="Babylon Health" />
         </div>
-        <div style={{ maxWidth: 600, margin: '24px auto' }}>
-          <ButtonList
-            options={[
-              { value: 'gp', label: 'GP' },
-              { value: 'therapist', label: 'Therapist' },
-              { value: 'physio', label: 'Physio' },
-              { value: 'specialist', label: 'Specialist' },
-            ]}
-            onChange={value =>
-              this.setState({ selectedAppointmentType: value })
-            }
-          />
 
+        <div className="app-header">
+          <h2 className="h6">New appointment</h2>
+          {this.state.user ? (
+            <div>
+              <img
+                src={this.state.user['avatar']}
+                className="avatar"
+                alt="${this.state.user['firstName']} Avatar"
+              />
+              <strong>
+                {this.state.user['firstName']} {this.state.user['lastName']}
+              </strong>
+            </div>
+          ) : null}
+        </div>
+        <div style={{ maxWidth: 600, margin: '24px auto' }}>
           <div>
-            <strong>Appointments</strong>
-            {slots.map(slot => (
-              <li
-                className="appointment-button"
-                onClick={() => {
-                  this.setState({ selectedAppointment: slot })
-                }}
-              >
-                {slot.time}
-              </li>
-            ))}
+            <strong>Consultant Type</strong>
+            <div>
+              <ButtonList
+                options={[
+                  { value: 'gp', label: 'GP' },
+                  { value: 'therapist', label: 'Therapist' },
+                  { value: 'physio', label: 'Physio' },
+                  { value: 'specialist', label: 'Specialist' },
+                ]}
+                onChange={value =>
+                  this.setState({ selectedConsultantType: value })
+                }
+              />
+            </div>
           </div>
+          <div>
+            <strong>Date & Time</strong>
+            <div>
+              <ButtonList
+                options={slots.map(slot => ({
+                  value: slot.time,
+                  label: this.formatDatetime(slot.time),
+                }))}
+                onChange={value => this.setState({ selectedTime: value })}
+              />
+            </div>
+          </div>
+          <div>
+            <strong>Appointment Type</strong>
+            <div>
+              <ButtonList
+                options={availableSlotsForTime.map(type => ({
+                  value: type,
+                  label: type[0].toUpperCase() + type.substring(1),
+                }))}
+                onChange={value =>
+                  this.setState({ selectedAppointmentType: value })
+                }
+              />
+            </div>
+          </div>
+
           <div>
             <strong>Notes</strong>
             <textarea />
@@ -88,8 +167,13 @@ class App extends Component {
                 /* TODO: submit the data */
               }}
             >
-              Book appointment
+              Book
             </div>
+          </div>
+          <div>
+            Consultant: {this.state.selectedConsultantType}
+            Time: {this.state.selectedTime}
+            Type: {this.state.selectedAppointmentType}
           </div>
         </div>
       </div>
